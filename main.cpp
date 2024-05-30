04ddd5e406d64b0bcab90fd6ae9297ceccdcf191
seekdude <rohan.nandu358@gmail.com>
2024-05-30 08:11:57 +0545

    so this chai works

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class pokemon{
+    public:
+        int type;
+        string color;
+};
+
+int main(){
+    pokemon pikachu,doraemon;
+    pikachu.type = 1;
+    pikachu.color = "yellow";
+
+}
